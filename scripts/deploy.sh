71e74b0f6a54ac110cb5885865505df41bb9db92
Nic Jackson <jackson.nic@gmail.com>
2017-06-30 14:08:36 +0100

    Added wait to create ssh connection

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 cd terraform
 ssh-keyscan 52.215.48.30 >> ~/.ssh/known_hosts
 ssh -N -i ~/.ssh/nomad_rsa -L 4646:internal-nomad-consul-internal-902604809.eu-west-1.elb.amazonaws.com:4646 ubuntu@52.215.48.30 &
+sleep 10
 TF_VAR_version=$TRAVIS_BUILD_ID terraform plan
 TF_VAR_version=$TRAVIS_BUILD_ID terraform apply
-killall ssh
