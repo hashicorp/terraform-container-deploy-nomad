b9e2b07660153a3aefcb7577cb52e5d5ce50b1de
Nic Jackson <jackson.nic@gmail.com>
2017-06-30 13:52:57 +0100

    Updated travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd $TRAVIS_BUILD_DIR/terraform
-ssh-keyscan 52.215.48.30 >>~/.ssh/known_hosts
+cd terraform
+ssh-keyscan 52.215.48.30 >> ~/.ssh/known_hosts
 ssh -N -i ~/.ssh/nomad_rsa -L 4646:internal-nomad-consul-internal-902604809.eu-west-1.elb.amazonaws.com:4646 ubuntu@52.215.48.30 &
 TF_VAR_version=$TRAVIS_BUILD_ID terraform plan
 TF_VAR_version=$TRAVIS_BUILD_ID terraform apply
