35a6c5ba9decdd5e554c327ba6761bfbe3987e95
Nic Jackson <jackson.nic@gmail.com>
2017-06-30 15:26:36 +0100

    Updated nomad server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -ev
 
 cd terraform
 ssh-keyscan 52.51.228.216 >> ~/.ssh/known_hosts
-ssh -N -i ~/.ssh/nomad_rsa -L 4646:internal-nomad-consul-internal-902604809.eu-west-1.elb.amazonaws.com:4646 ubuntu@52.51.228.216 &
+ssh -N -i ~/.ssh/nomad_rsa -L 4646:internal-nomad-consul-internal-1094868142.eu-west-1.elb.amazonaws.com:4646 ubuntu@52.51.228.216 &
 sleep 10
 TF_VAR_version=$TRAVIS_BUILD_ID terraform plan
 TF_VAR_version=$TRAVIS_BUILD_ID terraform apply
