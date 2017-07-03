f3957503afa44b84efb5bc46226568ba3970e958
Nic Jackson <jackson.nic@gmail.com>
2017-07-03 20:40:04 +0100

    Updated deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,6 @@ set -ev
 
 cd terraform
 
-# Create SSH Tunnel
-ssh-keyscan 52.51.228.216 >> ~/.ssh/known_hosts
-ssh -f -N -i ~/.ssh/nomad_rsa -L 4646:internal-nomad-consul-internal-1094868142.eu-west-1.elb.amazonaws.com:4646 ubuntu@52.51.228.216 & echo $! > nomad.pid
-
 # Run terraform
 TF_VAR_version=$TRAVIS_BUILD_ID terraform plan
 TF_VAR_version=$TRAVIS_BUILD_ID terraform apply
-
-# Cleanup
-kill $(cat nomad.pid)
-rm nomad.pid
-
